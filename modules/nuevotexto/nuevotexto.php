ca4e3939ee705afc87b5cbbff747675cdcac534f
aparis-webimpacto <aparis@webimpacto.es>
2022-05-16 10:25:58 +0200

    subida con tarea 3

---
+++ modules/nuevotexto/nuevotexto.php
@@ -0,0 +1,220 @@
+<?php
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class Nuevotexto extends Module
+{
+    protected $config_form = false;
+
+    public function __construct()
+    {
+        $this->name = 'nuevotexto';
+        $this->tab = 'administration';
+        $this->version = '1.0.0';
+        $this->author = 'Alba';
+        $this->need_instance = 0;
+
+        /**
+         * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
+         */
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('nuevotexto');
+        $this->description = $this->l('Modulo para añadir nuevo texto a los productos');
+
+        $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+    }
+
+    /**
+     * Don't forget to create update methods if needed:
+     * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update
+     */
+    public function install()
+    {
+        Configuration::updateValue('NUEVOTEXTO_LIVE_MODE', false);
+
+        return parent::install() &&
+            $this->registerHook('header') &&
+            $this->registerHook('backOfficeHeader');
+    }
+
+    public function uninstall()
+    {
+        Configuration::deleteByName('NUEVOTEXTO_LIVE_MODE');
+
+        return parent::uninstall();
+    }
+
+     //Instalamos el sql para agregar el campo adicional
+     public function installSql(){
+        //uno normal y otro con el editor especial
+        $sqlInstall = "ALTER TABLE " . _DB_PREFIX_ . "product "
+                . "ADD custom_field VARCHAR(255) NULL";
+        $sqlInstallLang = "ALTER TABLE " . _DB_PREFIX_ . "product_lang "
+                . "ADD custom_field_lang_wysiwyg TEXT NULL";
+
+        $cadenaSQl = Db::getInstance()->execute($sqlInstall);
+        $cadenaSQlLang = Db::getInstance()->execute($sqlInstallLang);
+
+        return $cadenaSQl && $cadenaSQlLang;
+    }
+
+    protected function unistallSql() {
+        $sqlInstall = "ALTER TABLE " . _DB_PREFIX_ . "product "
+                 . "DROP custom_field";
+         $sqlInstallLang = "ALTER TABLE " . _DB_PREFIX_ . "product_lang "
+                 . "DROP custom_field_lang_wysiwyg";
+  
+         $cadenaSQl = Db::getInstance()->execute($sqlInstall);
+         $cadenaSQlLang = Db::getInstance()->execute($sqlInstallLang);
+  
+         return $cadenaSQl && $cadenaSQlLang;
+     }
+
+    /**
+     * Load the configuration form
+     */
+    public function getContent()
+    {
+        /**
+         * If values have been submitted in the form, process.
+         */
+        if (((bool)Tools::isSubmit('submitNuevotextoModule')) == true) {
+            $this->postProcess();
+        }
+
+        $this->context->smarty->assign('module_dir', $this->_path);
+
+        $output = $this->context->smarty->fetch($this->local_path.'views/templates/admin/configure.tpl');
+
+        return $output.$this->renderForm();
+    }
+
+    /**
+     * Create the form that will be displayed in the configuration of your module.
+     */
+    protected function renderForm()
+    {
+        $helper = new HelperForm();
+
+        $helper->show_toolbar = false;
+        $helper->table = $this->table;
+        $helper->module = $this;
+        $helper->default_form_language = $this->context->language->id;
+        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
+
+        $helper->identifier = $this->identifier;
+        $helper->submit_action = 'submitNuevotextoModule';
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
+            .'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+
+        $helper->tpl_vars = array(
+            'fields_value' => $this->getConfigFormValues(), /* Add values for your inputs */
+            'languages' => $this->context->controller->getLanguages(),
+            'id_language' => $this->context->language->id,
+        );
+
+        return $helper->generateForm(array($this->getConfigForm()));
+    }
+
+    /**
+     * Create the structure of your form.
+     */
+    protected function getConfigForm()
+    {
+        return array(
+            'form' => array(
+                'legend' => array(
+                'title' => $this->l('Settings'),
+                'icon' => 'icon-cogs',
+                ),
+                'input' => array(
+                    array(
+                        'type' => 'switch',
+                        'label' => $this->l('Live mode'),
+                        'name' => 'NUEVOTEXTO_LIVE_MODE',
+                        'is_bool' => true,
+                        'desc' => $this->l('Use this module in live mode'),
+                        'values' => array(
+                            array(
+                                'id' => 'active_on',
+                                'value' => true,
+                                'label' => $this->l('Enabled')
+                            ),
+                            array(
+                                'id' => 'active_off',
+                                'value' => false,
+                                'label' => $this->l('Disabled')
+                            )
+                        ),
+                    ),
+                    array(
+                        'col' => 3,
+                        'type' => 'text',
+                        'prefix' => '<i class="icon icon-envelope"></i>',
+                        'desc' => $this->l('Enter a valid email address'),
+                        'name' => 'NUEVOTEXTO_ACCOUNT_EMAIL',
+                        'label' => $this->l('Email'),
+                    ),
+                    array(
+                        'type' => 'password',
+                        'name' => 'NUEVOTEXTO_ACCOUNT_PASSWORD',
+                        'label' => $this->l('Password'),
+                    ),
+                ),
+                'submit' => array(
+                    'title' => $this->l('Save'),
+                ),
+            ),
+        );
+    }
+
+    /**
+     * Set values for the inputs.
+     */
+    protected function getConfigFormValues()
+    {
+        return array(
+            'NUEVOTEXTO_LIVE_MODE' => Configuration::get('NUEVOTEXTO_LIVE_MODE', true),
+            'NUEVOTEXTO_ACCOUNT_EMAIL' => Configuration::get('NUEVOTEXTO_ACCOUNT_EMAIL', 'contact@prestashop.com'),
+            'NUEVOTEXTO_ACCOUNT_PASSWORD' => Configuration::get('NUEVOTEXTO_ACCOUNT_PASSWORD', null),
+        );
+    }
+
+    /**
+     * Save form data.
+     */
+    protected function postProcess()
+    {
+        $form_values = $this->getConfigFormValues();
+
+        foreach (array_keys($form_values) as $key) {
+            Configuration::updateValue($key, Tools::getValue($key));
+        }
+    }
+
+    /**
+    * Add the CSS & JavaScript files you want to be loaded in the BO.
+    */
+    public function hookBackOfficeHeader()
+    {
+        if (Tools::getValue('module_name') == $this->name) {
+            $this->context->controller->addJS($this->_path.'views/js/back.js');
+            $this->context->controller->addCSS($this->_path.'views/css/back.css');
+        }
+    }
+
+    /**
+     * Add the CSS & JavaScript files you want to be added on the FO.
+     */
+    public function hookHeader()
+    {
+        $this->context->controller->addJS($this->_path.'/views/js/front.js');
+        $this->context->controller->addCSS($this->_path.'/views/css/front.css');
+    }
+}
---
+++ themes/classic-rocket/templates/catalog/_partials/garantia_devolucion.tpl
@@ -0,0 +1,7 @@
+{block name='stock'}
+<div class="iconos_partebaja--product">
+    <img src="{$urls.img_url}certificado-de-garantia.png">
+    <img src="{$urls.img_url}devolucion-de-producto.png">
+    <img src="{$urls.img_url}certificado-de-garantia.png">
+</div>
+{/block}
